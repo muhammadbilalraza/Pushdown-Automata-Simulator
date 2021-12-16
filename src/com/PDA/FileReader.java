e3999457c24637978d89a5ea4a69d2e248381575
Muhammad Bilal Raza <muhammadbilalraza@outlook.com>
2021-12-16 04:29:57 +0300

    FileReader.java refactored.

--- src/com/PDA/FileReader.java
+++ src/com/PDA/FileReader.java
@@ -47,10 +47,6 @@ public class FileReader {
 	public FileReader (File fileObj) {
 		try {
 			Scanner reader = new Scanner(fileObj);
-			totalLines = 0;
-			transitionTableSize = 0;
-			noOfInputs = 0;
-
 
 			nInpVar = Integer.parseInt(reader.nextLine());
 			nSVar = Integer.parseInt(reader.nextLine());
@@ -62,9 +58,6 @@ public class FileReader {
 			setArrayListStr(stackAlphabets, reader.nextLine());
 			initSSymbol = reader.nextLine();
 			setArrayListInt(inAlphabets, reader.nextLine());
-
-			totalLines = 10;
-
 
 			String temp = "";
 			boolean transitionFlag = true;
@@ -80,18 +73,13 @@ public class FileReader {
 					inputFlag = true;
 				} //end if
 
-				if (transitionFlag) {
+				if (transitionFlag)
 					setArrayListStr(tTableTemp, temp);
-					transitionTableSize++; 
-					totalLines++;
-				} //end if
-
+				
 				//reading rest of the inputs
-				if (inputFlag) {
+				if (inputFlag)
 					setArrayListStr(inString, temp);
-					noOfInputs++;
-					totalLines++;
-				} //end if
+
 			} //end while
 
 			tTable = generateTuplesForTT(tTableTemp);
@@ -123,127 +111,30 @@ public class FileReader {
 	}
 
 // Getters & Setters
-
-	public int getnInpVar() {
-		return nInpVar;
-	}
-
-	public void setnInpVar(int nInpVar) {
-		this.nInpVar = nInpVar;
-	}
-
-	public int getnSVar() {
-		return nSVar;
-	}
-
-	public void setnSVar(int nSVar) {
-		this.nSVar = nSVar;
-	}
-
-	public int getnGStates() {
-		return nGStates;
-	}
-
-	public void setnGStates(int nGStates) {
-		this.nGStates = nGStates;
-	}
-
-	public int getnStates() {
-		return nStates;
-	}
-
-	public void setnStates(int nStates) {
-		this.nStates = nStates;
-	}
-
 	public ArrayList<String> getStates() {
 		return states;
 	}
-
-	public void setStates(ArrayList<String> states) {
-		this.states = states;
-	}
-
 	public String getStartState() {
 		return startState;
 	}
-
-	public void setStartState(String startState) {
-		this.startState = startState;
-	}
-
 	public ArrayList<String> getGoalStates() {
 		return goalStates;
 	}
-
-	public void setGoalStates(ArrayList<String> goalStates) {
-		this.goalStates = goalStates;
-	}
-
 	public ArrayList<String> getStackAlphabets() {
 		return stackAlphabets;
 	}
-
-	public void setStackAlphabets(ArrayList<String> stackAlphabets) {
-		this.stackAlphabets = stackAlphabets;
-	}
-
 	public String getInitSSymbol() {
 		return initSSymbol;
 	}
-
-	public void setInitSSymbol(String initSSymbol) {
-		this.initSSymbol = initSSymbol;
-	}
-
 	public ArrayList<Integer> getInAlphabets() {
 		return inAlphabets;
 	}
-
-	public void setInAlphabets(ArrayList<Integer> inAlphabets) {
-		this.inAlphabets = inAlphabets;
-	}
-
 	public ArrayList<String> gettTableTemp() {
 		return tTableTemp;
 	}
-
-	public void settTableTemp(ArrayList<String> tTableTemp) {
-		this.tTableTemp = tTableTemp;
-	}
-
 	public ArrayList<String> getInString() {
 		return inString;
 	}
-
-	public void setInString(ArrayList<String> inString) {
-		this.inString = inString;
-	}
-
-	public int getTotalLines() {
-		return totalLines;
-	}
-
-	public void setTotalLines(int totalLines) {
-		this.totalLines = totalLines;
-	}
-
-	public int getTransitionTableSize() {
-		return transitionTableSize;
-	}
-
-	public void setTransitionTableSize(int transitionTableSize) {
-		this.transitionTableSize = transitionTableSize;
-	}
-
-	public int getNoOfInputs() {
-		return noOfInputs;
-	}
-
-	public void setNoOfInputs(int noOfInputs) {
-		this.noOfInputs = noOfInputs;
-	}
-
 	public List<ArrayList<String>> gettTable() {
 		return tTable;
 	}
@@ -258,43 +149,17 @@ public class FileReader {
 			System.out.println();
 		}
 	}
-
 	public void printArrayListStr(ArrayList<String> list){
 		for (int i = 0; i<list.size(); i++){
 			System.out.print(list.get(i) + " ");
 		}
 		System.out.println();
 	}
-
-
 	public void printArrayListInt(ArrayList<Integer> list){
 		for (int i=0; i<list.size(); i++){
 			System.out.print(list.get(i) + " ");
 		}
 		System.out.println();
 	}
-
-
-	public void printFile () {
-		System.out.println("Total lines : " + totalLines);
-		System.out.println("Transaction Table Size : " + transitionTableSize);
-		System.out.println("Inputs size : " + noOfInputs);
-
-		System.out.println(getnInpVar());
-		System.out.println(getnSVar());
-		System.out.println(getnGStates());
-		System.out.println(getnStates());
-		printArrayListStr(getStates());
-		System.out.println(getStartState());
-		printArrayListStr(getGoalStates());
-		printArrayListStr(getStackAlphabets());
-		System.out.println(getInitSSymbol());
-		printArrayListInt(getInAlphabets());
-//		printArrayListStr(gettTable());
-		printTuples(tTable);
-		printArrayListStr(getInString());
-		System.out.print(getInString().size());
-	}
-
 
 }
